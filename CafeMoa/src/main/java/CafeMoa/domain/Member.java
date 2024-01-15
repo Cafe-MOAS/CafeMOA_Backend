aeafb7422753585223ee4ea5b3fe6626969f1aee
SangWookie <swcube520@gmail.com>
2024-01-15 18:45:02 +0900

    feat : Member 엔티티 추가

--- CafeMoa/src/main/java/CafeMoa/domain/Member.java
+++ CafeMoa/src/main/java/CafeMoa/domain/Member.java
@@ -1,4 +1,38 @@
 package CafeMoa.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Members")
 public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "nickname", nullable = false, unique = true)
+    private String nickname;
+
+    @OneToMany(mappedBy = "member_id")
+    private List<Cafe> favoriteCafeList;
+
+    @OneToMany(mappedBy = "member_id")
+    private List<Review> reviewList;
+
+    @OneToMany(mappedBy = "member_id2")
+    private List<Review> likeReviewList;
 }
