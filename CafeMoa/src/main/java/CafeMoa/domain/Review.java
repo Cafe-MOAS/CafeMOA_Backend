6d73499f9a034ef3636c8e013ffce557b8ec0fec
youngroky <139459645+youngroky@users.noreply.github.com>
2024-01-23 22:17:19 +0900

    feat: review entity 추가
    - 요소들 추가

--- CafeMoa/src/main/java/CafeMoa/domain/Review.java
+++ CafeMoa/src/main/java/CafeMoa/domain/Review.java
@@ -1,4 +1,41 @@
 package CafeMoa.domain;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Table(name ="Review_Entity")
+
 
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private Long reviewId;
+
+    @Column(name = "body", nullable = false)
+    private String body;
+
+    @Column(name = "like_count", nullable = true)
+    private int likeCount;
+
+    @Column(name = "rating", nullable = false)
+    private int rating;
+
+    @OneToMany(mappedBy = "review", cascade = CascadeType.ALL)
+    @Column(name = "category_list")
+    private List<Category> categoryList;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @Column(name = "cafe", nullable = false)
+    private Cafe cafe;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @Column(name = "author", nullable = false)
+    private Member author;
 }
